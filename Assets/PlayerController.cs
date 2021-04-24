1295b1c4f94a9528d4d32c44d3e5bc5b5e6f9442
Pedro Santos de Mendonca <peumendonca@gmail.com>
2021-04-24 15:01:30 +0100

    Left and right movement

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,25 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Mover : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     Rigidbody2D body;
-    public float speed = 1;
+    public float speed = 2;
 
     // Start is called before the first frame update
     void Start()
     {
         body = GetComponent<Rigidbody2D>();
-        
-        print("Start");
     }
 
     // Update is called once per frame
     void Update()
     {
-        print("Frame");
-        body.velocity = new Vector2(speed, body.velocity.y);
-        
+        Move();
     }
+
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float xDelta = x * speed;
+        body.velocity = new Vector2(xDelta, body.velocity.y);
+    }
+
 }
