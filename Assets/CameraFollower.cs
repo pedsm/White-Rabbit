46f2070919c60abc28bd5b0b267e02fac6546d4a
Pedro Santos de Mendonca <peumendonca@gmail.com>
2021-04-24 23:54:30 +0100

    Up and down + Better map

--- Assets/CameraFollower.cs
+++ Assets/CameraFollower.cs
@@ -7,6 +7,7 @@ public class CameraFollower : MonoBehaviour
     // Start is called before the first frame update
     public Transform player;
     public Vector3 offset;
+    public float downOffset = 2f;
     void Start()
     {
         
@@ -15,9 +16,12 @@ public class CameraFollower : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       float y = Input.GetAxisRaw("Vertical");
+       float yDiff = downOffset * y;
+
        transform.position = new Vector3(
            player.position.x + offset.x,
-           player.position.y + offset.y,
+           player.position.y + offset.y + yDiff,
            offset.z
         );
     }
