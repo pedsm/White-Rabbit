66283dcb5f496180960094f9e184ea328bfd42eb
Pedro Santos de Mendonca <peumendonca@gmail.com>
2021-04-24 16:08:40 +0100

    Tilemap added

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,36 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
-  Rigidbody2D body;
-  public float speed = 2;
+    Rigidbody2D body;
+    public float speed = 2;
+    public float jumpForce = 5;
 
-  // Start is called before the first frame update
-  void Start()
-  {
-    body = GetComponent<Rigidbody2D>();
-  }
-
-  // Update is called once per frame
-  void Update()
-  {
-    Move();
-    Jump();
-  }
-
-  void Move()
-  {
-    float x = Input.GetAxisRaw("Horizontal");
-    float xDelta = x * speed;
-    body.velocity = new Vector2(xDelta, body.velocity.y);
-  }
-
-  void Jump()
-  {
-    if (Input.GetKeyDown(KeyCode.Space))
+    // Start is called before the first frame update
+    void Start()
     {
-      float jumpVelocity = 10f;
-      body.velocity = Vector2.up * jumpVelocity;
+        body = GetComponent<Rigidbody2D>();
     }
-  }
 
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        Jump();
+    }
+
+    void Move()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float xDelta = x * speed;
+        body.velocity = new Vector2(xDelta, body.velocity.y);
+    }
+
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            float jumpVelocity = 10f;
+            body.velocity = Vector2.up * jumpVelocity;
+        }
+
+    }
 }
