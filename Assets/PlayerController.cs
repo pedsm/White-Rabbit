d2cd66d1aa6977465b51824a3583b5e6fa441f15
Lawand Othman <lwnd@pm.me>
2021-04-24 16:03:14 +0100

    add jump function

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -3,26 +3,36 @@ using System.Collections.Generic;
 using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
-    Rigidbody2D body;
-    public float speed = 2;
+  Rigidbody2D body;
+  public float speed = 2;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        body = GetComponent<Rigidbody2D>();
-    }
+  // Start is called before the first frame update
+  void Start()
+  {
+    body = GetComponent<Rigidbody2D>();
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Move();
-    }
+  // Update is called once per frame
+  void Update()
+  {
+    Move();
+    Jump();
+  }
 
-    void Move()
+  void Move()
+  {
+    float x = Input.GetAxisRaw("Horizontal");
+    float xDelta = x * speed;
+    body.velocity = new Vector2(xDelta, body.velocity.y);
+  }
+
+  void Jump()
+  {
+    if (Input.GetKeyDown(KeyCode.Space))
     {
-        float x = Input.GetAxisRaw("Horizontal");
-        float xDelta = x * speed;
-        body.velocity = new Vector2(xDelta, body.velocity.y);
+      float jumpVelocity = 10f;
+      body.velocity = Vector2.up * jumpVelocity;
     }
+  }
 
 }
