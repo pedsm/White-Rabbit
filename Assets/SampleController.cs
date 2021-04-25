9259efe9876feb94e61d9b324056545977240a7e
rish1603 <rish1603@gmail.com>
2021-04-25 20:34:55 +0100

    OST Intro transition, modified samples, modified heli prefab
    heli's now have less audio range

--- Assets/SampleController.cs
+++ Assets/SampleController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public enum SoundName
 {
@@ -8,10 +9,13 @@ public enum SoundName
     JUMP,
     LAND,
     DAMAGED,
-    HIT
+    HIT,
+    ARPEG
 }
 
 public class SampleController : MonoBehaviour {
+
+    public AudioMixer arpegio;
 
     private AudioSource[] sounds;
 
@@ -32,6 +36,8 @@ public class SampleController : MonoBehaviour {
         damaged = sounds[(int)SoundName.DAMAGED];
         hit = sounds[(int)SoundName.HIT];
 
+        StartCoroutine(StartFade(arpegio, "volume", 20f, 1f));
+
     }
 
     public void playSound(SoundName soundName) {
@@ -42,8 +48,25 @@ public class SampleController : MonoBehaviour {
         if(volume > 1) {
             volume = 1f;
         }
-
         sounds[(int)soundName].volume = volume;
         sounds[(int)soundName].Play();
+    }
+
+     public static IEnumerator StartFade(AudioMixer audioMixer, string exposedParam, float duration, float targetVolume)
+    {
+        float currentTime = 0;
+        float currentVol;
+        audioMixer.GetFloat(exposedParam, out currentVol);
+        currentVol = Mathf.Pow(10, currentVol / 20);
+        float targetValue = Mathf.Clamp(targetVolume, 0.0001f, 1);
+
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+            float newVol = Mathf.Lerp(currentVol, targetValue, currentTime / duration);
+            audioMixer.SetFloat(exposedParam, Mathf.Log10(newVol) * 20);
+            yield return null;
+        }
+        yield break;
     }
 }
